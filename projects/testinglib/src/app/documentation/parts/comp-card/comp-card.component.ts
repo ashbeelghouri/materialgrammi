9920f51190ec2c4c6cb84943f8bacdcf7d5e7a48
Ashbeel Ghouri <ashbeelghouri@protonmail.com>
2022-04-01 18:17:15 +0500

    prod

--- projects/testinglib/src/app/documentation/parts/comp-card/comp-card.component.ts
+++ projects/testinglib/src/app/documentation/parts/comp-card/comp-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SeoService } from 'projects/materialgrammi/src/public-api';
 
 @Component({
   selector: 'app-comp-card',
@@ -17,18 +18,59 @@ export class CompCardComponent implements OnInit {
 
   title = "Card";
 
-  code = `<mg-card>
-            <p class="blue pad-10 text-grey1">Lorem Ipsum</p>
-          </mg-card>`;
+  code = `<div class="row">
+  <div class="col m3 l3 s12">
+      <mg-card class="margin-5 pink radiusT-5">
+          <mg-image class=" radiusT-5" src="https://picsum.photos/300/300?random=1"></mg-image>
+          <h3 class="text-grey2 pad-5 padY-10">Title</h3>
+          <p class="pad-5 text-grey1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cupiditate pariatur nihil vitae, tenetur ullam accusamus a, reiciendis ipsa omnis eligendi sint quam quia! Recusandae qui necessitatibus corrupti hic deserunt?</p>
+          <div class="text-right">
+              <mg-button class="pad-5" btnStyle="link" theme="lite">read more</mg-button>
+          </div>
+      </mg-card>
+  </div>
+  <div class="col m3 l3 s12">
+      <mg-card class="margin-5 grey8 radiusT-5">
+          <mg-image class=" radiusT-5" src="https://picsum.photos/300/300?random=4"></mg-image>
+          <h3 class="text-grey2 pad-5 padY-10">Title</h3>
+          <p class="pad-5 text-grey1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cupiditate pariatur nihil vitae, tenetur ullam accusamus a, reiciendis ipsa omnis eligendi sint quam quia! Recusandae qui necessitatibus corrupti hic deserunt?</p> 
+          <div class="text-right">
+              <mg-button class="pad-5" btnStyle="link" theme="lite">read more</mg-button>
+          </div>
+      </mg-card>
+  </div>
+  <div class="col m3 l3 s12">
+      <mg-card class="margin-5 blue radiusT-5">
+          <mg-image class=" radiusT-5" src="https://picsum.photos/300/300?random=2"></mg-image>
+          <h3 class="text-grey2 pad-5 padY-10">Title</h3>
+          <p class="pad-5 text-grey1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cupiditate pariatur nihil vitae, tenetur ullam accusamus a, reiciendis ipsa omnis eligendi sint quam quia! Recusandae qui necessitatibus corrupti hic deserunt?</p> 
+          <div class="text-right">
+              <mg-button class="pad-5" btnStyle="link" theme="lite">read more</mg-button>
+          </div>
+          
+      </mg-card>
+  </div>
+  <div class="col m3 l3 s12">
+      <mg-card class="margin-5 orange radiusT-5">
+          <mg-image class=" radiusT-5" src="https://picsum.photos/300/300?random=3"></mg-image>
+          <h3 class="text-grey2 pad-5 padY-10">Title</h3>
+          <p class="pad-5 text-grey1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit cupiditate pariatur nihil vitae, tenetur ullam accusamus a, reiciendis ipsa omnis eligendi sint quam quia! Recusandae qui necessitatibus corrupti hic deserunt?</p> 
+          <div class="text-right">
+              <mg-button class="pad-5" btnStyle="link" theme="lite">read more</mg-button>
+          </div>
+      </mg-card>
+  </div>
+</div>`;
 
   optionsApplied = {}
 
   outputOptions = [];
 
 
-  constructor() { }
+  constructor(private seo: SeoService) { }
 
   ngOnInit(): void {
+    this.seo.setTitle("Materialgrammi | Card");
   }
 
 }
--- projects/testinglib/src/app/documentation/parts/comp-collapse/comp-collapse.component.ts
+++ projects/testinglib/src/app/documentation/parts/comp-collapse/comp-collapse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SeoService } from 'projects/materialgrammi/src/public-api';
 
 @Component({
   selector: 'app-comp-collapse',
@@ -38,9 +39,10 @@ export class CompCollapseComponent implements OnInit {
     type: "boolean"
   }];
 
-  constructor() { }
+  constructor(private seo: SeoService) { }
 
   ngOnInit(): void {
+    this.seo.setTitle("Materialgrammi | Collapse");
   }
 
 }
